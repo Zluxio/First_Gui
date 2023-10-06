e8fe70c1a3528a35a3d31f08333e14d23035d1dc
Zluxio <boltonjaden6872@gmail.com>
2023-10-05 20:02:31 -0400

    Idk what I am doing, but im making it slowly.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <cmath>
+#include <QVector>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -14,16 +16,39 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_switch_2_toggled(bool checked)
+void MainWindow::on_checkBox1_toggled(bool checked)
 {
-    if(ui->switch_2->en){
-        ui->lcdNumber->display(1);
-    } else {
-        ui->lcdNumber->display(0);
+
+    if(checked == true) {
+
+    bits.replace(0,1);
+    ui->lcdNumber->display(1);
+
+    } else if (checked == false) {
+
+    bits.replace(0,0);
+
+    ui->lcdNumber->display(0);
     }
 
 }
 
 
 
+int MainWindow::decode(){
 
+
+
+    for(int i = 0; i < 9; i++){
+
+
+
+        if (bits[i] == 0){
+            return 0;
+        } else if (bits[i] == 1) {
+            return 1;
+
+        }
+
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,22 +1,33 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <vector>
+#include <iostream>
 #include <QMainWindow>
+#include <QVector>
+using namespace std;
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+    QVector<int> bits = {0,0,0,0,0,0,0,0};
+    int total;
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    int decode();
+
 private slots:
-    void on_switch_2_toggled(bool checked);
+
+
+    void on_checkBox1_toggled(bool checked);
 
 private:
     Ui::MainWindow *ui;
